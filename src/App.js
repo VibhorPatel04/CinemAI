b287fff8693e3c3e9524d16ce82dfca69d0fb77e
Vibhor Patel <vibhor.patrl04@gmail.com>
2024-04-12 22:21:03 +0530

    create react app and tailwind config

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App container">
+      <h1 className="text-3xl font-bold text-red-800">Start Project</h1>
+    </div>
+  );
+}
+
+export default App;
